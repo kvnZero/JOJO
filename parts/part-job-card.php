6df47b4fda1bb1cbd9b0a96dfa5ea5c3626fe379
kvnZero <abigeater@163.com>
2024-10-16 16:51:15 +0800

    update change card style

--- parts/part-job-card.php
+++ parts/part-job-card.php
@@ -40,8 +40,8 @@ $job_type_icon = $job_type ? wpjam_get_term_thumbnail_url($job_type[0]) : null;
                 </div>
             </div>
           </div>
-          <div class="d-none d-md-block col-md-1">
-            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+          <div class="d-none d-md-flex col-md-1 align-items-center justify-content-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="60%" height="60%" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
             </svg>
           </div>
