79f7cf6f50fd2f83a9f134036ca68d3d59169dfc
kvnZero <abigeater@163.com>
2024-10-16 16:10:01 +0800

    add ajax_url in head

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript">var ajaxurl = "<?php echo admin_url('admin-ajax.php');?>";</script>
     <?php
     wp_head();
     $site_secondary_color = get_theme_mod( 'site_secondary_color', '212529bf' );
